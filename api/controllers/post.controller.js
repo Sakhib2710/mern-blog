ef2b4e86e99cba5ce8cd4e79c16dce945b36477b
Sakhib Ali <Sakhib2710@users.noreply.github.com>
2024-05-20 18:12:10 +0530

    spelling correction

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -63,7 +63,7 @@ export const getPosts = async (req, res, next) => {
 
 export const deletePost = async (req, res, next) => {
   if (!req.user.isAdmin || req.user.id != req.params.userId) {
-    return next(errorHandler(403, 'You are not allowed to delelte this post'));
+    return next(errorHandler(403, 'You are not allowed to delete this post'));
   }
   try {
     await Post.findByIdAndDelete(req.params.postId);
